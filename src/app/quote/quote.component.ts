afc7ce7c0f3588cae79ae66a554028bd2bea72f4
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-13 07:12:08 +0300

    makes changes to the class quote components

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
-    new Quote(5,'Solve math homework','Damn Math'),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
+    new Quote(1, 'Human spirit', 'The human spirit must prevail over technology to allow humans be in charge.'),
+    new Quote(2,'Technology','Technology  the knack of so arranging  that we don’t have to experience it.'),
+    new Quote(3,'The great myth','The great myth of our times is that technology is communication.'),
+    new Quote(4,'Communication','Communications tools don’t get socially interesting until they get technologically boring.'),
+    new Quote(5,'Code and people','Programs must be written for people to read, and only incidentally for machines to execute.'),
+    new Quote(6,'Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.'),
   ];
 
   toggleDetails(index:number){
