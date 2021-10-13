9b8571ba5db4be9c6df2d1d962f0137c294325d2
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-13 17:06:58 +0300

    remove complete button

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -12,9 +12,6 @@ export class QuoteDetailsComponent implements OnInit {
   @Input() quote!: Quote;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  quoteComplete(complete:boolean){
-    this.isComplete.emit(complete);
-  }
 
   quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
