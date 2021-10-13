5d58006a9cab8f12ed8a56556831cef336eb7afd
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-13 13:42:11 +0300

    adds the pipes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,13 +8,21 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Human spirit', 'The human spirit must prevail over technology to allow humans be in charge.'),
-    new Quote(2,'Technology','Technology  the knack of so arranging  that we don’t have to experience it.'),
-    new Quote(3,'The great myth','The great myth of our times is that technology is communication.'),
-    new Quote(4,'Communication','Communications tools don’t get socially interesting until they get technologically boring.'),
-    new Quote(5,'Code and people','Programs must be written for people to read, and only incidentally for machines to execute.'),
-    new Quote(6,'Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.'),
+    new Quote(1, 'Human spirit', 'The human spirit must prevail over technology to allow humans be in charge.',new Date(2019,9,14)),
+    new Quote(2,'Technology','Technology  the knack of so arranging  that we don’t have to experience it.',new Date(2019,6,9)),
+    new Quote(3,'The great myth','The great myth of our times is that technology is communication.',new Date(2019,1,12)),
+    new Quote(4,'Communication','Communications tools don’t get socially interesting until they get technologically boring.',new Date(2019,1,12)),
+    new Quote(5,'Code and people','Programs must be written for people to read, and only incidentally for machines to execute.',new Date(2019,1,12)),
+    new Quote(6,'Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.',new Date(2019,1,12)),
   ];
+
+
+  addNewQuote(quote: Quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
 
   toggleDetails(index:number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
@@ -40,3 +48,5 @@ export class QuoteComponent implements OnInit {
   ngOnInit(): void {
   }
 }
+
+
