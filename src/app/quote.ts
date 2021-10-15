4cbf13a2d922b69ab01c5b8f2f713f50f5c20538
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-10-15 10:27:10 +0300

    redoes quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,15 @@
+
 export class Quote {
     // id!: number;
     // name!: string;
     // description!: string;
-
-
-
+  
+  
+  
     showDescription: boolean;
     constructor(public id: number,public name: string,public author : string,public description: string ,public completeDate: Date , ){
       this.showDescription=false; // id!: number;
     }
     
-}
+  }
+  
